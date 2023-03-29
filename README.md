7fcf6b2e9dcbb85f73e141fec8c8e320bf1a4cdb
Bennie Kowalski <1pokealex@gmail.com>
2023-03-29 14:39:50 -0500

    paycheck app Translated from java library to C++ -> DONE

--- README.md
+++ README.md
@@ -8,8 +8,8 @@ Somewhere to practice C++ and other C base programming language.
 -----------------------------------|-------------------------------------------------------------------
              Commands                   		        Commands Definition
 -----------------------------------|-------------------------------------------------------------------
-g++ --version     			gets the version of the package installed for g++ complier
-apt install g++   			installs g++ complier for C++ 
+g++ --version     			            gets the version of the package installed for g++ complier
+apt install g++   			            installs g++ complier for C++ 
 g++ "FileName".cpp -o "FileName"        complies the code into a .exe file
 ./"FileName"                            runs the .exe file
 
@@ -18,5 +18,5 @@ g++ "FileName".cpp -o "FileName"        complies the code into a .exe file
              Commands                                   Commands Definition
 -----------------------------------|-------------------------------------------------------------------
 g++ --version                           gets the version of the package installed for g++ complier
-g++ "FileName".cpp			complies the .cpp to a .exe file
-"FileName".exe				Runs the .exe file 
+g++ "FileName".cpp			            complies the .cpp to a .exe file
+./"FileName".exe				        Runs the .exe file 
--- paycheck.cpp
+++ paycheck.cpp
@@ -30,7 +30,7 @@ int main()
             cout << endl;
 
             // Array to store data
-            double pay_raise[18] = {};
+            double pay_raise[17] = {};
 
             // User Data
             cout << "What is your current hourly rate" << endl;
@@ -45,13 +45,13 @@ int main()
 
             // Output for Hourly Wage
             cout << "------------------------------------------------------------------------" << endl;
-            cout << "                    Pay Raise Summary                                   " << endl;
+            cout << "                          Pay Raise Summary                             " << endl;
             cout << "------------------------------------------------------------------------" << endl;
-            cout << "                    | Hourly Wage |              " << endl;
-            printf("The Current Hourly Wage                          : %.2f\n", pay_raise[0]);
-            printf("The Pay Raise %                                  : %.0f\n", pay_raise[1]);
-            printf("The Pay Raise Increase Dallor Amount             : %.2f\n", pay_raise[3]);
-            printf("The Hourly Wage after the Pay Raise is           : %.2f\n", pay_raise[4]);
+            cout << "                   | Hourly Wage |              " << endl;
+            printf("The Current Hourly Wage                          : $ %.2f\n", pay_raise[0]);
+            printf("The Pay Raise %                                  :   %.0f\n", pay_raise[1]);
+            printf("The Pay Raise Increase Dallor Amount             : $ %.2f\n", pay_raise[3]);
+            printf("The Hourly Wage after the Pay Raise is           : $ %.2f\n", pay_raise[4]);
             cout << endl; // Spacing
 
             // Processing for Weekly Income
@@ -61,33 +61,33 @@ int main()
 
             // Output for Weekly Income
             cout << "                   | Weekly Income |              " << endl;
-            printf("The Current Weekly Income                        : %.2f\n", pay_raise[5]);
-            printf("The Pay Raise Weekly Income                      : %.2f\n", pay_raise[6]);
-            printf("The Difference between the two Weekly Incomes    : %.2f\n", pay_raise[7]);
+            printf("The Current Weekly Income                        : $ %.2f\n", pay_raise[5]);
+            printf("The Pay Raise Weekly Income                      : $ %.2f\n", pay_raise[6]);
+            printf("The Difference between the two Weekly Incomes    : $ %.2f\n", pay_raise[7]);
             cout << endl; // Spacing
 
             // Processing for Bi-Weekly Income
-            pay_raise[8] = pay_raise[5] * 2;             // Base-line Original Weekly Amount
-            pay_raise[9] = pay_raise[6] * 2;             // Base-line New Weekly Amount
+            pay_raise[8] = pay_raise[5] * 2;             // Base-line Original BI- eekly Amount
+            pay_raise[9] = pay_raise[6] * 2;             // Base-line New Bi-Weekly Amount
             pay_raise[10] = pay_raise[9] - pay_raise[8]; // Difference Between the two
 
-            // Output for Bi-Weekly
+            // Output for Bi-Weeklypa
             cout << "                   | Bi-Weekly Income |              " << endl;
-            printf("The Current Bi-Weekly Income                     : %.2f\n", pay_raise[8]);
-            printf("The Pay Raise Bi-Weekly Income                   : %.2f\n", pay_raise[9]);
-            printf("The Difference between the two Weekly Incomes    : %.2f\n", pay_raise[10]);
+            printf("The Current Bi-Weekly Income                     : $ %.2f\n", pay_raise[8]);
+            printf("The Pay Raise Bi-Weekly Income                   : $ %.2f\n", pay_raise[9]);
+            printf("The Difference between the two Weekly Incomes    : $ %.2f\n", pay_raise[10]);
             cout << endl; // Spacing
 
             // Processing for Monthly Income
-            pay_raise[11] = pay_raise[8] * 2;              // Base-line Orignal Weekly Amount
-            pay_raise[12] = pay_raise[9] * 2;              // Base-line New Weekly Amount
+            pay_raise[11] = pay_raise[5] * 4;              // Base-line Orignal Weekly Amount
+            pay_raise[12] = pay_raise[6] * 4;              // Base-line New Weekly Amount
             pay_raise[13] = pay_raise[12] - pay_raise[11]; // Difference Between the two
 
             // Output for Monthly Income
             cout << "                   | Monthly Income |              " << endl;
-            printf("The Current Monthly Income                     : %.2f\n", pay_raise[8]);
-            printf("The Pay Raise Monthly Income                   : %.2f\n", pay_raise[9]);
-            printf("The Difference between the two Monthly Incomes : %.2f\n", pay_raise[10]);
+            printf("The Current Monthly Income                       : $ %.2f\n", pay_raise[11]);
+            printf("The Pay Raise Monthly Income                     : $ %.2f\n", pay_raise[12]);
+            printf("The Difference between the two Monthly Incomes   : $ %.2f\n", pay_raise[13]);
             cout << endl; // Spacing
 
             // Processing for Annual Income
@@ -97,10 +97,58 @@ int main()
 
             // Output for Annual Income
             cout << "                   | Annual Income |              " << endl;
-            printf("The Current Annual Income                     : %.2f\n", pay_raise[14]);
-            printf("The Pay Raise Annual Income                   : %.2f\n", pay_raise[15]);
-            printf("The Difference between the two Annual Incomes : %.2f\n", pay_raise[16]);
+            printf("The Current Annual Income                     : $ %.2f\n", pay_raise[14]);
+            printf("The Pay Raise Annual Income                   : $ %.2f\n", pay_raise[15]);
+            printf("The Difference between the two Annual Incomes : $ %.2f\n", pay_raise[16]);
             cout << endl; // Spacing
+        }
+        if (userInput == "paycheck")
+        {
+            // Array to the store the Data
+            double paycheck[12] = {};
+
+            // User Data
+            cout << "What is current hourly wage?" << endl;
+            cin >> paycheck[0];
+            cout << "How many hours did you work last paycheck?" << endl;
+            cin >> paycheck[1];
+
+            // Processing for Overtime
+            if (paycheck[0] > 80)
+            {
+                paycheck[2] = paycheck[1] - 80;                          // getting the value of how many hours above the normal 80 hours
+                paycheck[3] = paycheck[0] * 80;                          // normal wage * max of 80 hours
+                paycheck[4] = (paycheck[0] * paycheck[2]) * 0.5;         // $ value of the half time  on the over time hours only
+                paycheck[5] = (paycheck[0] * paycheck[2]) * 1.0;         // $ value time for over time
+                paycheck[6] = paycheck[3] + (paycheck[4] + paycheck[5]); // paycheck with overtime include
+                paycheck[7] = paycheck[6] / 4.5;                         // Taxes value "Roughly"
+                paycheck[8] = paycheck[6] - paycheck[7];                 // Net Payment
+
+                // Output for Overtime
+                cout << "---------------------------------------------- " << endl;
+                cout << "|             Overtime Paycheck              | " << endl;
+                cout << "---------------------------------------------- " << endl;
+                printf("       Gross Income                : $ %.2f\n", paycheck[6]);
+                printf("       Taxes (Roughly @ 4.5)       : $ %.2f\n", paycheck[7]);
+                printf("       Net Payment (Roughly)       : $ %.2f\n", paycheck[8]);
+                cout << endl;
+            }
+            else
+            {
+                // Paycheck without Overtime
+                paycheck[9] = paycheck[0] * paycheck[1];   // $ value of Wage * hours
+                paycheck[10] = paycheck[9] / 4.5;          // Taxes  value "Roughly"
+                paycheck[11] = paycheck[9] - paycheck[10]; // Net Payment
+
+                // Output
+                cout << "---------------------------------------------- " << endl;
+                cout << "|                  Paycheck                  | " << endl;
+                cout << "---------------------------------------------- " << endl;
+                printf("     Gross Income            : $ %.2f\n", paycheck[9]);
+                printf("     Taxes (Roughly @ 4.5)   : $ %.2f\n", paycheck[10]);
+                printf("     Net Payment             : $ %.2f\n", paycheck[11]);
+                cout << endl;
+            }
         }
     }
 }
