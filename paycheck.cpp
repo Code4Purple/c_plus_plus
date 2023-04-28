dffbcd85827893a55e8e82a4cef5c07ae79ae91b
Bennie Kowalski <1pokealex@gmail.com>
2023-04-28 01:36:34 -0500

    making the paycheck app within c++ repo -> bookmark

---
+++ paycheck.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+void annualIncome(double x){
+	x * 52; // one week * 52 weeks in a year = annual income
+};
+
+int main(){
+ annualIncome(80.22);
+ return 0;
+}
