79a06c898ede3695c38036b176d071b30f307930
Bennie Kowalski <1pokealex@gmail.com>
2023-04-28 01:59:59 -0500

    got the basics of the code working just needs to add more functions for it then an command line UI to it -- save

--- paycheck.cpp
+++ paycheck.cpp
@@ -2,11 +2,34 @@
 
 using namespace std;
 
-void annualIncome(double x){
-	x * 52; // one week * 52 weeks in a year = annual income
+double oneWeek;
+double biWeekly;
+
+double weeklyIncome(double hourlyRate, int minHours)
+{
+
+	oneWeek = hourlyRate * minHours;
+	// printf("Weekly Income: %.2f", oneWeek);
+	return oneWeek;
 };
 
-int main(){
- annualIncome(80.22);
- return 0;
+double biWeeklyIncome(double weekly)
+{
+	biWeekly = weekly * 2;
+	return biWeekly;
+};
+void annualIncome(double weekly)
+{
+	double annual;
+	annual = weekly * 52;
+	printf("Annual Income: %.2f\n", annual); // one week * 52 weeks in a year = annual income
+};
+
+int main()
+{
+	weeklyIncome(18.45, 80);
+	printf("Weekly Income: %.2f \n", oneWeek);
+	biWeeklyIncome(oneWeek);
+	printf("Bi-Weekly Income: %.2f\n", biWeekly);
+	return 0;
 }
