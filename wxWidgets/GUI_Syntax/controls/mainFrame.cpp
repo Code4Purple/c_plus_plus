dabaaf030d17c707356557e244f4d8a8a102faea
Bennie Kowalski <1pokealex@gmail.com>
2023-05-19 20:46:28 -0500

    Deleting .vs

--- wxWidgets/GUI_Syntax/controls/mainFrame.cpp
+++ wxWidgets/GUI_Syntax/controls/mainFrame.cpp
@@ -22,7 +22,7 @@ MainFrame::MainFrame(const wxString& title) : wxFrame(nullptr, wxID_ANY, title) 
 	choice->Select(0);
 
 
-	wxSpinCtrl* spinCtrl = new wxSpinCtrl(panel, wxID_ANY, "Enter Title", wxPoint(550, 375), wxSize(100, -1));
+	wxSpinCtrl* spinCtrl = new wxSpinCtrl(panel, wxID_ANY, "Enter Title 1", wxPoint(550, 375), wxSize(100, -1));
 	
 	wxListBox* listBox = new wxListBox(panel, wxID_ANY,  wxPoint(150, 475), wxSize(100, -1), choices);
 
