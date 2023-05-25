9be047e8dc87343a88240404734533136446e8a6
Bennie Kowalski <1pokealex@gmail.com>
2023-05-25 00:58:39 -0500

    Savepoint

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 ASIO C++ Library
 
+Video Bookmark: https://youtu.be/2hNdkYInj4g
--- example1.cpp
+++ example1.cpp
@@ -1,8 +1,14 @@
+// Cross platform
 #include <iostream>
+
+// Windows
+// #define ASIO_STANDALONE
+// #include <asio.hpp>
+// #inlcude <asio/ts/buffer.hpp>
+// #inlcude <asio/ts/internet.hpp>
 
 int main()
 {
-    ;
 
     return 0;
 }
