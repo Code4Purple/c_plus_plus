b68d6d62176382bc7f38d3a9e623ced070c4ebb5
Bennie Kowalski <1pokealex@gmail.com>
2023-06-01 21:46:06 -0500

    Counting lines - IDEA

--- invites/README.md
+++ invites/README.md
@@ -1,1 +1,5 @@
 https://cplusplus.com/forum/general/31920/
+
+
+couting line example :
+https://stackoverflow.com/questions/3072795/how-to-count-lines-of-a-file-in-c
--- invites/dataread.cpp
+++ invites/dataread.cpp
@@ -15,17 +15,17 @@ void friendsSort()
     char fileName[20] = "ID.txt";
     // cout << "Enter the Name of File that would like to sort from to make a friends list: ";
     // cin >> fileName;
-    char fileName2[20] = "temp.friends";
+    char fileName2[20] = "friends.temp ";
     // Open data Streams to read and write // start of friends store 1st
     DataStream1.open(fileName, ios::in);
     DataStream2.open(fileName2, ios::out | ios::trunc);
     // Making sure thses are the stream are open to be able start store
     if (DataStream1.is_open())
     {
-        printf("%s is open \n", fileName);
+        printf("%s is open() \n", fileName);
         if (DataStream2.is_open())
         {
-            printf("%s is open \n", fileName2);
+            printf("%s is open() \n", fileName2);
 
             while (getline(DataStream1, line))
             {
@@ -39,10 +39,72 @@ void friendsSort()
                     if (item == "friends")
                     {
                         DataStream2 << line << endl;
-                        printf("Data -> %s \n", line);
+                        cout << "Data -> " << line << endl;
                     }
                 }
-                // cout << item << endl;
+            }
+        }
+        else
+        {
+            printf(" ERROR -> Data Stream %s FAILED \n", fileName2);
+        }
+    }
+    else
+    {
+        printf(" ERROR -> Data Stream %s FAILED \n", fileName);
+    }
+    DataStream2.close();
+    if (DataStream2.is_open())
+    {
+        printf("Error -> %s.close() FAILED \n", fileName2);
+    }
+    else
+    {
+        printf("%s.close() -> PASSED\n", fileName2);
+    }
+    DataStream1.close();
+    if (DataStream1.is_open())
+    {
+        printf("Error -> %s.close() FAILED \n", fileName);
+    }
+    else
+    {
+        printf("%s.close() -> PASSED", fileName);
+    }
+}
+void friendHS()
+{
+    // making the temp data into the human readable files
+    char fileName[20] = "friends.temp";
+    char fileName2[20] = "friends_list.temp";
+    // Open data Streams to read and write // start of friends document creation
+    DataStream1.open(fileName, ios::in);
+    DataStream2.open(fileName2, ios::out | ios::trunc);
+    // Making sure these are the stream are open to be able start store
+    if (DataStream1.is_open())
+    {
+        printf("%s is open() -> PASSED \n", fileName);
+        if (DataStream2.is_open())
+        {
+            printf("%s is open() -> PASSED \n", fileName2);
+            while (getline(DataStream1, line))
+            {
+
+                stringstream ss;
+                ss.str(line);
+                string item;
+                while (getline(ss, item, ':'))
+                {
+                    // if (line.length() > 0)
+                    if (item == "friends")
+                    {
+                        // << line << endl;
+                        // printf("Data -> %s \n", line);
+                        continue;
+                    }
+                    DataStream2 << item << endl;
+                    cout << "Data -> " << item << endl;
+                }
             }
         }
         else
@@ -76,9 +138,9 @@ void friendsSort()
 
 int main()
 {
-
-    // making the temp data into the human readable files
-    char fileName[20] = "temp.friends";
+    // friendsSort();
+    // friendHS();
+    / making the temp data into the human readable files char fileName[20] = "friends.temp";
     char fileName2[20] = "friends_list.temp";
     // Open data Streams to read and write // start of friends document creation
     DataStream1.open(fileName, ios::in);
@@ -86,10 +148,10 @@ int main()
     // Making sure these are the stream are open to be able start store
     if (DataStream1.is_open())
     {
-        printf("%s.is_open() -> PASSED \n", fileName);
+        printf("%s is open() -> PASSED \n", fileName);
         if (DataStream2.is_open())
         {
-            printf("%s.is_open() -> PASSED \n", fileName2);
+            printf("%s is open() -> PASSED \n", fileName2);
             while (getline(DataStream1, line))
             {
 
@@ -105,8 +167,8 @@ int main()
                         // printf("Data -> %s \n", line);
                         continue;
                     }
-
-                    // cout << "Line  -> " << line << endl;
+                    DataStream2 << item << endl;
+                    cout << "Data -> " << item << endl;
                 }
             }
         }
@@ -137,4 +199,5 @@ int main()
     {
         printf("%s.close() -> PASSED", fileName);
     }
+}
 }
