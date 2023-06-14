32305c1226a87f292c1d112a233835ecc2e19050
Bennie Kowalski <1pokealex@gmail.com>
2023-06-14 02:05:55 -0500

    setup the mogas array

--- fuelFarm/include.h
+++ fuelFarm/include.h
@@ -1,5 +1,12 @@
 #include <iostream>
-// #include <>
-#include <fstream>
+#include <vector>
+#include <string>
 
 using namespace std;
+
+void await(int input)
+{
+    for (int i = 0; i < input; i++)
+    {
+    }
+}
---
+++ fuelFarm/mogas.cpp
@@ -0,0 +1,27 @@
+#include "include.h"
+
+vector<vector<double>> mogas = {
+    {0.000, 0},
+    {0.125, 0},
+    {0.250, 1},
+    {0.375, 1},
+    {0.500, 2},
+    {0.625, 3},
+    {0.750, 3},
+    {0.875, 4},
+    {1.000, 1}};
+
+int main()
+{
+    cout << "Test Print of data to the console!" << endl;
+    await(715827882);
+    cout << "Size of the Mogas Array -> " << mogas.size() << endl;
+    for (int i = 0; i < mogas.size(); i++)
+    {
+        for (int j = 0; j < mogas[i].size(); j++)
+        {
+            cout << mogas[i][j] << endl;
+            await(715827882);
+        }
+    }
+}
