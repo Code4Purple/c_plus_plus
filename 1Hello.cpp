14328a1ed8cb87405fc16ee1a5409b35890b5772
Bennie Kowalski <1pokealex@gmail.com>
2023-06-30 16:35:34 -0500

    .\3ArithmeticOperators.cpp

--- 1Hello.cpp
+++ 1Hello.cpp
@@ -3,6 +3,10 @@
 
 int main(){
     cout << "Hello.cpp Data ----> " ;
+    cout << "(Cout version) ";
     cout << "Hello World!" << endl;
+
+    printf("(printf version) \n");
+    printf("Hello World!");
     return 0;
 }
---
+++ 3ArithmeticOperators.cpp
@@ -0,0 +1,30 @@
+/**
+ *    Arithmetic Operators
+ *      +    addition 
+ *      -    subtraction
+ *      *    multiplication 
+ *      /    division
+ *      %    modulus 
+ * 
+ */
+
+#include "include.h"
+
+int main(){
+    
+    int a = 10;
+    int b = 3;
+    int sum = a + b;
+    int difference = a - b;
+    int product = a * b;
+    int quotient = a / b;
+    int remainder = a % b;
+
+    printf("Addition or Sum of              %d & %d = %d \n", a , b ,sum);
+    printf("Subtraction or Difference of    %d & %d = %d \n", a , b ,difference);
+    printf("Multiplication or Product of    %d & %d = %d \n", a , b ,product);
+    printf("Divison or Quotient of          %d & %d = %d \n", a , b ,quotient);
+    printf("Modulus or Remainder of         %d & %d = %d \n", a , b ,remainder);
+    
+    return 0;
+}
