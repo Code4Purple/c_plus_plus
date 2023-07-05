a1c8fec325a37a7c215619815c8c7a618ecfd07b
Bennie Kowalski <1pokealex@gmail.com>
2023-07-04 20:41:43 -0500

    Data_handler.cpp start

---
+++ Machine Learning C++/include/data.h
@@ -0,0 +1,30 @@
+#ifndef __DATA_H
+#define __DATA_H
+
+#include <vector>
+#include <iostream>
+//using namespace std; 
+
+class data {
+    std::vector<uint8_t> * feature_vector;
+    uint8_t label;
+    int enum_label;
+
+    public:
+    data();
+    ~data();
+
+    void set_feature_vector(std::vector<uint8_t> *);
+    void append_to_feature_vector(uint8_t);
+    void set_label(uint8_t);
+    void set_enumerated_label(int);
+    void set_distance(double val);
+
+    int get_feature_vector_size();
+    uint8_t get_label();
+    uint8_t get_enmerated_label();
+
+    std::vector<uint8_t> *get_feature_vector();
+};
+
+#endif
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 https://www.udemy.com/
+
+cmake .. -G "Unix Makefiles" --> make files in windows
