9687f7242f4ddb7b12f1842ecafc5a67a148eb80
Bennie Kowalski <1pokealex@gmail.com>
2023-07-06 21:26:56 -0500

    Upload so more notes

---
+++ Machine Learning C++/MINIST_ML/build/CMakeFiles/libraries.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/libraries.dir/src/data.cpp.obj"
+  "CMakeFiles/libraries.dir/src/data.cpp.obj.d"
+  "CMakeFiles/libraries.dir/src/data_handler.cpp.obj"
+  "CMakeFiles/libraries.dir/src/data_handler.cpp.obj.d"
+  "liblibraries.a"
+  "liblibraries.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/libraries.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
