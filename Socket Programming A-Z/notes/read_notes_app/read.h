5ea926c08d060c22e519d5771c8b5d6152cec981
Bennie Kowalski <65255810+Code4Purple@users.noreply.github.com>
2023-07-07 05:59:37 -0500

    Seting up a read me app for notes in Socket

---
+++ Socket Programming A-Z/notes/read_notes_app/read.h
@@ -0,0 +1,27 @@
+#include <vector>
+#include <iostream>
+#include <string>
+#include <fstream>
+using namespace std;
+
+
+class read{
+
+    public:
+    read();
+    ~read();
+
+    string line;
+
+    void await(){
+        for (int i = 0; i < INT_MAX; i++){
+            
+        }
+
+    }
+
+    char readfile [100];
+    fstream readfile;
+    vector<string> list = {};
+    
+};
