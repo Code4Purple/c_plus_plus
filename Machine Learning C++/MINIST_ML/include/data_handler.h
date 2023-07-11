f52b482b7820f552f7241bb12f0e2381c64a80cc
Bennie Kowalski <1pokealex@gmail.com>
2023-07-11 17:05:52 -0500

    Rebuild of ML

---
+++ Machine Learning C++/MINIST_ML/include/data_handler.h
@@ -0,0 +1,16 @@
+#ifndef __DATA_HANDLER_H
+#define __DATA_HANDLER_H
+
+#include <vector>
+#include <string>
+#include <fstream>
+#include <map>
+#include <unordered_set>
+#include <iostream>
+#include "data.h"
+
+class data_handler{
+
+};
+
+#endif
