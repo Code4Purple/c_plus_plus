7c75de5e28e45328a0eee1e77dc24abde454734f
Bennie Kowalski <65255810+Code4Purple@users.noreply.github.com>
2023-08-02 02:10:04 -0500

    2-2 is done

--- The Complete C++ Developer Course/2Repetition.cpp
+++ The Complete C++ Developer Course/2Repetition.cpp
@@ -1,4 +1,5 @@
-#include "include.h";
+#include "include.h"
+
 
 /**
  *      Repetition Control Statements
@@ -11,5 +12,31 @@
 
 
 int main(){
-        
+    printf("\n      Repetition Control Statements");
+    printf("\nWhile Loop\n");
+    // Continuation Loop
+    int count = 0;
+    while(count <= 10){
+        std::cout << count << " ";
+        count++;
+    }    
+
+    // Post Test Loop // Execution is always one before checking it's continuation loop
+    std::cout << std::endl;
+    printf("\nDo-While Loop\n");
+    double count2 = 0;
+    do {
+        std::cout << count2 << " " ;
+        count2 = count2 + 0.1;
+    }while (count2 <= 10);
+
+    //
+    std::cout << std::endl;
+    printf("\nFor Loop\n");
+    for(double i = 0; i <= 10; i = i + 0.20){
+        std::cout << i << " ";
+    }
+    
+    
+    return 0;
 }
