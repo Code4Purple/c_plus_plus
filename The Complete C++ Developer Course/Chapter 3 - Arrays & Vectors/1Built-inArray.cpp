e8502538c4e047dd7672a073ae2042cd4221f111
Bennie Kowalski <1pokealex@gmail.com>
2023-08-03 03:47:54 -0500

    Savepoint

--- The Complete C++ Developer Course/Chapter 3 - Arrays & Vectors/1Built-inArray.cpp
+++ The Complete C++ Developer Course/Chapter 3 - Arrays & Vectors/1Built-inArray.cpp
@@ -1,13 +1,14 @@
 #include "include.h"
 
 int main(){
-
+    printf("\n      Built-in Array\n");
     // 1st Way of Array def
     //int array1[5];
 
     // 2nd way of array def
     const int ARRAY_SIZE = 5;
     int array2[ARRAY_SIZE];
+    std::string names[4] = {"Bob", "Jimmy", "Johnny", "Timmy"};
 
     array2[0] = 15;
     array2[1] = 20;
@@ -15,12 +16,17 @@ int main(){
     array2[3] = 6;
     array2[4] = 55;
 
+    
     std::cout << "Value of each cells of array2 : ";
     for(int i = 0; i < ARRAY_SIZE; i++){
         std::cout << array2[i] << " " ;
     }
 
-
+    std::cout << std::endl;
+    std::cout << "Value of each cells of array of names : ";
+    for(std::string name: names){
+        std::cout << name << " ";
+    }
 
     return 0;
 
