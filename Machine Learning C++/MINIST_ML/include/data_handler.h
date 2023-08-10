91bab523abd0554010ceef435b1614432edabd67
Bennie Kowalski <65255810+Code4Purple@users.noreply.github.com>
2023-08-10 02:01:42 -0500

    data_handler.h --SETUP

---
+++ Machine Learning C++/MINIST_ML/include/data_handler.h
@@ -0,0 +1,16 @@
+#ifndef __DATA_HANDLER_H
+#define __DATA_HANDLER_H
+
+#include <iostream>
+#include <string>
+#include <map>
+#include <fstream>
+#include <unordered_set>
+#include <vector>
+#include "data.h"
+
+class data_handler{
+    
+};
+
+#endif
