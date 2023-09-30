294c3370adb64ce6e748f9266599d53542113146
Bennie Kowalski <65255810+Code4Purple@users.noreply.github.com>
2023-09-30 09:20:58 -0500

    GUI -> wxWidgets learning file -- upload

---
+++ wxWidgets/GUI_Syntax/2_controls/mainFrame.h
@@ -0,0 +1,8 @@
+#pragma once
+#include <wx/wx.h> //WXWIN
+class MainFrame : public wxFrame
+{
+public:
+	// barbones frame of the appliction title bar definition with basic pass-inputs
+	MainFrame(const wxString &title);
+};
---
+++ wxWidgets/GUI_Syntax/4_events/app.cpp
@@ -0,0 +1,20 @@
+#include "App.h"
+#include "MainFrame.h"
+#include "wx/wx.h"
+
+wxIMPLEMENT_APP(App);
+
+bool App::OnInit()
+{
+
+	// Creating & Titling the Appliction
+	MainFrame* mainFrame = new MainFrame("C++ GUI - Static Evbents");
+	// Size of the intail window
+	mainFrame->SetClientSize(800, 600);
+	// Where it starts at on your monitor
+	mainFrame->Center();
+	// Allows to Print to the UI in interaction section
+	mainFrame->Show();
+	// Allows the Window to Start & stay open with the mainFrame structure
+	return true;
+}
