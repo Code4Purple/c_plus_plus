683a03bbb6cb43364ec5d459ddfbd1d0973b8346
Bennie Kowalski <65255810+Code4Purple@users.noreply.github.com>
2023-10-08 15:04:20 -0500

    M_ML_2

---
+++ Machine Learning C++/MINIST_ML_2/include/data_handler.hpp
@@ -0,0 +1,12 @@
+#ifndef __DATA_HANDLER_H
+#define __DATA_HANDLER_H
+
+
+
+class data_handler {
+    
+};
+
+#endif
+
+//https://youtu.be/E1K9SZCm0fQ?list=TLPQMDgxMDIwMjO9YHu5RMFw8g&t=749
---
+++ boring_folder/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+
+std::vector<int> counting;
+int user_max_size = 0;
+
+
+void maxInt(){
+    for(int i = 0; i < user_max_size + 1; i++){
+        counting.push_back(i);
+    }
+};
+void maxDouble(){
+    for(double i = 00.00; i < user_max_size + 1; i += .10){
+        counting.push_back(i);
+    }
+};
+
+void printData(){
+    std::cout << "Printing the data..." << std::endl;
+    for (const auto& element : counting) {
+        std::cout << element << " ";
+    }
+        std::cout << std::endl;
+};
+
+int main(){
+    std::cout << "How for do want to count? " << std::endl;
+    std::cin >> user_max_size;
+    std::cout << "Gathering Data..." << std::endl;
+    std::cout << "Size of Vector : " << counting.size() << std::endl;
+    maxInt();
+    //maxDouble();
+    std::cout << "Got all data gathered! " << std::endl;
+    std::cout << "Size of Vector : " << counting.size() << std::endl;
+    printData();
+
+
+}
