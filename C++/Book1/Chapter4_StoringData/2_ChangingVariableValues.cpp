884579814a7e064397339543ecc520c8e61dae44
Bennie Kowalski <1pokealex@gmail.com>
2024-09-20 20:39:11 -0500

    init

---
+++ C++/Book1/Chapter4_StoringData/2_ChangingVariableValues.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main(){
+
+    // 1st round of the value
+    int x;
+    x = 5;
+    cout << "Orginal Value : " << x << endl;
+
+    // 2nd round of the value
+    x = 10;
+    cout << "Changed Value : " << x << endl;
+
+    return 0;
+}
---
+++ C++/Book1/Chapter4_StoringData/8_UsingTheConditionalToDoComparisons.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    // Declaring Variables
+    int a, b;
+    a = 10;
+    b = 20;
+    string result;
+
+    // Conditional Statement
+    result = a == b ? "equal" : "not equal";
+
+    cout << result << endl;
+
+
+    return 0;
+}
