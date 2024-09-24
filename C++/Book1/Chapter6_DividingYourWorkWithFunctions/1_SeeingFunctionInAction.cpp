c7902736c5305ad67e9e87871b5ec8fce347a89f
Bennie Kowalski <1pokealex@gmail.com>
2024-09-24 14:24:07 -0500

    Chapter 6 Bookmark @ 6.5

---
+++ C++/Book1/Chapter6_DividingYourWorkWithFunctions/1_SeeingFunctionInAction.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main(){
+    
+    // abs() function
+    cout << fabs(-10.5) << endl;
+    cout << fabs(10.5) << endl;
+
+
+    return 0;
+}
