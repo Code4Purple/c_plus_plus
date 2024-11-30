44f06af3e4a9c5e0fd89e00133396eff4d288b0c
Bennie Kowalski <1pokealex@gmail.com>
2024-11-30 12:22:12 -0600

    so far

---
+++ micrograd/python-version/value.py
@@ -0,0 +1,19 @@
+class Value:
+    def __init__(self, data):
+        self.data = data
+    
+    def __repr__(self):
+        return f"Value(data={self.data})"
+    
+    def __add__(self, other):
+        return Value(self.data + other.data)
+    
+    def __mul__(self, other):
+        return Value(self.data * other.data)
+    
+
+a = Value(2.0)
+b = Value(-3.0)
+c = Value(10.0)
+
+print(a*b+c)
