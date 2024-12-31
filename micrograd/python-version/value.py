775dbd3a278d7731c5bc00eb6c5384014ec982d4
Alex Kowalski <1pokealex@gmail.com>
2024-12-31 09:55:12 -0600

    got the subtract and divison working

--- micrograd/python-version/value.py
+++ micrograd/python-version/value.py
@@ -11,9 +11,16 @@ class Value:
     def __mul__(self, other):
         return Value(self.data * other.data)
     
+    def __sub__(self, other):
+        return Value(self.data - other.data)
+    
+    def __truediv__(self, other):
+        return Value(self.data / other.data)
+    
 
 a = Value(2.0)
 b = Value(-3.0)
 c = Value(10.0)
 
-print(a + b * c)  # Value(data=-28.0)
+print(a + b * c - c)  # Value(data=-28.0)
+print(c/a)  # Value(data=5.0)
