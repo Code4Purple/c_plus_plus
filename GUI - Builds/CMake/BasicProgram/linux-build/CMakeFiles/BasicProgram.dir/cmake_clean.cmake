0b44133870a40f3ab680b73590061e390d668336
Code4Purple <1pokealex@gmail.com>
2025-01-30 08:29:07 -0600

    added linux version of the cmake BasicProgram

---
+++ GUI - Builds/CMake/BasicProgram/linux-build/CMakeFiles/BasicProgram.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/BasicProgram.dir/link.d"
+  "BasicProgram"
+  "BasicProgram.pdb"
+  "CMakeFiles/BasicProgram.dir/main.cpp.o"
+  "CMakeFiles/BasicProgram.dir/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BasicProgram.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ GUI - Builds/CMake/BasicProgram/linux-build/CMakeFiles/BasicProgram.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for BasicProgram.
+# This may be replaced when dependencies are built.
