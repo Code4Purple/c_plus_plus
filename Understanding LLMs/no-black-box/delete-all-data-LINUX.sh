0927be1a83a59f729e2fa21701888f1df922b1e8
Code4Purple <1pokealex@gmail.com>
2025-03-12 23:30:17 -0500

    server is working

--- Understanding LLMs/no-black-box/delete-all-data-LINUX.sh
+++ Understanding LLMs/no-black-box/delete-all-data-LINUX.sh
@@ -2,20 +2,20 @@
 
 echo " REMOVED json FOLDER"
 echo " REMOVED ALL DATA  : json"
-rm -r data/dataset/json
+sudo rm -r data/dataset/json
 sleep 6
 
 echo " REMOVED img FOLDER"
 echo " REMOVED ALL DATA  : img"
-rm -r data/dataset/img
+sudo rm -r data/dataset/img
 sleep 7
 
 echo " REMOVED samples.json FILE"
 echo " REMOVED DATA FILE: samples.json"
-rm data/dataset/samples.json
+sudo rm data/dataset/samples.json
 sleep 8
 
 echo " REMOVED js_objects FOLDER"
 echo " REMOVED DATA FILE: samples.js" 
-rm -r common/js_objects
+sudo rm -r common/js_objects
 sleep 3
