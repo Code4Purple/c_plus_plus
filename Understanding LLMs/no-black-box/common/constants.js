0eabd6cd3b1a0268b215aacc91b8e79ed9bdf06f
Code4Purple <1pokealex@gmail.com>
2025-03-12 20:56:52 -0500

    FIXED NAMES OF FILES

--- Understanding LLMs/no-black-box/common/constants.js
+++ Understanding LLMs/no-black-box/common/constants.js
@@ -5,7 +5,7 @@ constants.RAW_DIR=constants.DATA_DIR+"/raw";
 constants.DATASET_DIR=constants.DATA_DIR+"/dataset";
 constants.JSON_DIR=constants.DATASET_DIR+"/json";
 constants.IMG_DIR=constants.DATASET_DIR+"/img";
-constants.SAMPLES=constants.DATASET_DIR+"/samples.jscon";
+constants.SAMPLES=constants.DATASET_DIR+"/samples.json";
 
 if(typeof module!=='undefined'){
     module.exports=constants;
