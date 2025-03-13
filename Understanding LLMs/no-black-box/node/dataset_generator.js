9712b45a3d05496ab722062fd69fbbd050228f4e
Code4Purple <1pokealex@gmail.com>
2025-03-12 20:28:28 -0500

    gitignore

--- Understanding LLMs/no-black-box/node/dataset_generator.js
+++ Understanding LLMs/no-black-box/node/dataset_generator.js
@@ -1,5 +1,6 @@
 const draw = require('../common/draw.js');
 const constants = require('../common/constants.js');
+const utils = require('../common/utils.js');
 
 const {createCanvas} = require('canvas');
 const canvas = createCanvas(400,400);
@@ -38,8 +39,8 @@ fileNames.forEach(fn => {
             drawings[label]
         );
 
-
-
+        // print progress
+        utils.printProgress(id, fileNames.length*8);
         id++;
     }
 });
