3f4d0400a2698310f5f73a1dad3810b30e38ea2c
Alex Kowalski <1pokealex@gmail.com>
2025-03-20 19:06:38 -0500

    update .sh and .bat

---
+++ Understanding LLMs/no-black-box/node/node-linux.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+
+echo "Back-End Windows UI- Version 1"
+sleep 2
+
+value=0
+
+while [ $value -lt 10 ]; do
+    echo "Please select the option you want to perform"
+    sleep 2
+    echo "         0. Exit"
+    sleep 2
+    echo "         1. Reset the Data to Zero"
+    sleep 2
+    echo "         2. Node Rebuild"
+    sleep 2
+    echo "         3. Build the Data Structure - FULL"
+    sleep 2
+    echo " "
+    sleep 2
+    read -p "Enter the option: " input
+    echo " "
+    value=$((value + 1))
+
+    if [ "$input" -eq 0 ]; then
+        echo "Exiting the program"
+        exit
+    fi
+
+    if [ "$input" -eq 1 ]; then
+        echo "Resetting the Data to Zero"
+        sleep 2
+        echo "Removing the data within the dataset folder..."
+        cd ../data/dataset || exit
+        echo "REMOVED THE IMG FOLDER"
+        rm -rf img
+        echo "REMOVED THE JSON FOLDER"
+        rm -rf json
+        rm -f sample.json
+        echo "REMOVED THE FEATURES.JSON FILE"
+        rm -f features.json
+        echo " "
+        echo "Now Removing the common/js_objects folder..."
+        sleep 2
+
+        cd ../../common || exit
+        echo "REMOVED THE JS_OBJECTS FOLDER"
+        rm -rf js_objects
+        sleep 2
+
+        echo "DATA RESET TO ZERO"
+        sleep 2
+        echo " "
+        cd ../node || exit
+    fi
+
+    if [ "$input" -eq 2 ]; then
+        echo "Node Rebuild"
+        sleep 2
+        echo "Rebuilding the node modules..."
+        npm rebuild
+        echo " "
+        echo "NODE REBUILD COMPLETE"
+    fi
+
+    if [ "$input" -eq 3 ]; then
+        echo "Building the Data Structure - FULL"
+        sleep 2
+        cd ../data/dataset || exit
+        echo "Creating the img folder..."
+        mkdir -p img
+        sleep 2
+        echo "Creating the json folder..."
+        mkdir -p json
+        sleep 2
+        echo "Creating the js_objects folder..."
+        cd ../../common || exit
+        mkdir -p js_objects
+        sleep 2
+
+        cd ../node || exit
+        echo "Starting the data_generator.js file..."
+        node data_generator.js
+        echo " "
+        node feature_extractor.js
+        echo " "
+        echo "DATA STRUCTURE BUILD COMPLETE With Full Data"
+    fi
+done
---
+++ Understanding LLMs/no-black-box/node/node-windows.bat
@@ -0,0 +1,101 @@
+@echo off
+
+echo "Back-End Windows UI- Version 1"
+timeout /t 2 /nobreak >nul
+
+set value=0
+
+:while_loop
+:: check the condition
+if %value% LSS 10 (
+    echo "Please select the option you want to perform"
+    timeout /t 2 /nobreak >nul
+    echo "         0. Exit"
+    timeout /t 2 /nobreak >nul
+    echo "         1. Reset the Data to Zero"
+    timeout /t 2 /nobreak >nul
+    echo "         2. Node Rebuild"
+    timeout /t 2 /nobreak >nul
+    echo "         3. Build the Data Structure - FULL"
+    timeout /t 2 /nobreak >nul
+    echo " "
+    timeout /t 2 /nobreak >nul
+    set /p input="Enter the option: "
+    echo " "
+    set /a value=%value% + 1
+
+)
+
+:: what happens after the condition is met
+
+if %input%==0 (
+    echo "Exiting the program"
+    exit
+)
+
+
+if %input%==1 (
+    echo "Resetting the Data to Zero"
+    timeout /t 2 /nobreak >nul
+    echo "Removing the data within the dataset folder..."
+    cd ../data/dataset
+    echo "REMOVED THE IMG FOLDER"
+    rmdir img
+    echo "REMOVED THE JSON FOLDER"
+    rmdir img /s /q
+    timeout /t 2 /nobreak >nul
+    rmdir json /s /q
+    del sample.json
+    echo "REMVOED THE FEATURES.JSON FILE"
+    del features.json
+    echo " "
+    echo "Now Removing the common/js_objects folder..."
+    timeout /t 2 /nobreak >nul
+
+    cd ../../common
+    echo "REMOVED THE JS_OBJECTS FOLDER"
+    rmdir js_objects
+    timeout /t 2 /nobreak >nul
+
+    echo "DATA RESET TO ZERO"
+    timeout /t 2 /nobreak >nul
+    echo " "
+    cd ../node
+    goto while_loop
+)
+
+if %input%==2 (
+    echo "Node Rebuild"
+    timeout /t 2 /nobreak >nul
+    echo "Rebuilding the node modules..."
+    npm rebuild
+    echo " "
+    echo "NODE REBUILD COMPLETE"
+    goto while_loop
+)
+
+if %input%==3 (
+    echo "Building the Data Structure - FULL"
+    timeout /t 2 /nobreak >nul
+    cd ../data/dataset
+    echo "Creating the img folder..."
+    mkdir img
+    timeout /t 2 /nobreak >nul
+    echo "Creating the json folder..."
+    mkdir json
+    timeout /t 2 /nobreak >nul
+    echo "Creating the js_objects folder..."
+    cd ../../common
+    mkdir js_objects
+    timeout /t 2 /nobreak >nul
+    
+    
+    echo "Starting the data_generator.js file..."
+    cd ../node
+    node data_generator.js
+    echo " "
+    node feature_extractor.js
+    echo " "
+    echo "DATA STRUCTURE BUILD COMPLETE With Full Data"
+    goto while_loop
+)
