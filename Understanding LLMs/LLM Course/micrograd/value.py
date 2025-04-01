1e7975cb1a308fd62bd82d8c908a1c994465b20d
Code4Purple <1pokealex@gmail.com>
2025-04-01 05:57:30 -0500

    childern in value.py

--- Understanding LLMs/LLM Course/micrograd/value.py
+++ Understanding LLMs/LLM Course/micrograd/value.py
@@ -1,18 +1,19 @@
 
 class Value:
     
-    def __init__(self,data):
+    def __init__(self,data, _childern=()):
         self.data = data
+        self._prev = set(_childern)
     
     def __repr__(self):
         return f"Value(data={self.data})"
     
     def __add__(self,other):
-        out = Value(self.data + other.data)    
+        out = Value(self.data + other.data, (self, other))    
         return out
     
     def __mul__(self,other):
-        out = Value(self.data * other.data) 
+        out = Value(self.data * other.data, (self, other)) 
         return out
     
     #def 
@@ -21,4 +22,6 @@ class Value:
 a= Value(2.0)
 b = Value(-3.0)
 c = Value(10.0)
-print(a*b + c) # Value(data=30)
+d = a * b + c
+print(d)       # Value(data=4.0)
+print(d._prev) # {Value(data=10.0), Value(data=-6.0)}
