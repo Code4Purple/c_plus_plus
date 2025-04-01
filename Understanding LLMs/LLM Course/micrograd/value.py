7e93dd05ec58dab7c5f0a4e15bd8e7316a67bed1
Code4Purple <1pokealex@gmail.com>
2025-04-01 12:05:34 -0500

    _op

--- Understanding LLMs/LLM Course/micrograd/value.py
+++ Understanding LLMs/LLM Course/micrograd/value.py
@@ -1,19 +1,20 @@
 
 class Value:
     
-    def __init__(self,data, _childern=()):
+    def __init__(self,data, _childern=(), _op=''):
         self.data = data
         self._prev = set(_childern)
+        self._op = _op
     
     def __repr__(self):
         return f"Value(data={self.data})"
     
     def __add__(self,other):
-        out = Value(self.data + other.data, (self, other))    
+        out = Value(self.data + other.data, (self, other),'+')    
         return out
     
     def __mul__(self,other):
-        out = Value(self.data * other.data, (self, other)) 
+        out = Value(self.data * other.data, (self, other), '*') 
         return out
     
     #def 
@@ -23,5 +24,6 @@ a= Value(2.0)
 b = Value(-3.0)
 c = Value(10.0)
 d = a * b + c
-print(d)       # Value(data=4.0)
+print(d)       #  Value(data=4.0)
 print(d._prev) # {Value(data=10.0), Value(data=-6.0)}
+print(d._op)   # +
