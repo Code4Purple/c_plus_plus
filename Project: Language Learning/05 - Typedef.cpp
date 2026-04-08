2a5c0a571ecd254db88a52bb5bd660cd0469f82f
Code4Purple <1pokealex@gmail.com>
2026-04-08 12:58:15 -0500

    Bookmark @ 05

---
+++ Project: Language Learning/05 - Typedef.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main(){
+
+    /*
+        typedef = reserved keyword used to create an additional name (alias)
+        for another data type. New identifier for an existing type,
+        helps with readability and reduces typos.
+    */
+
+    // Bookmark : https://youtu.be/-TkoO8Z07hI?list=PLmZlEEZLifKuSr0hrFgx2KrWz2d7xJs5P&t=1933
+
+    return 0;
+}
