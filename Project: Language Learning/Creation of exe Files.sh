ee1d28d480339c632c5be105ba893777c6e5e7f9
Code4Purple <1pokealex@gmail.com>
2026-04-10 12:30:19 -0500

    .sh command fiile to create all the .exe files for the chapter!

---
+++ Project: Language Learning/Creation of exe Files.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+# Just add the new completed chapter to the list below to auto all the chapters exe creation
+
+g++ -o "01 - Hello World".exe "01 - Hello World".cpp
+g++ -o "02 - Variables & Basic Data Types".exe "02 - Variables & Basic Data Types".cpp
+g++ -o "03 - Const Keyword".exe "03 - Const Keyword".cpp
+g++ -o "04 - Namespaces".exe "04 - Namespaces".cpp
+g++ -o "05 - Typedef & type aliases".exe "05 - Typedef & type aliases".cpp
+#g++ -o "06 - Arithmetic Operators".exe "06 - Arithmetic Operators".cpp
+
