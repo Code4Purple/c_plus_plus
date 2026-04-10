7e9b17df0df60f21dfdd8cf98d2a3bc8322b9926
Code4Purple <1pokealex@gmail.com>
2026-04-10 11:43:25 -0500

    05 -- is done && 06 bookmarked!

--- Project: Language Learning/05 - Typedef.cpp
+++ Project: Language Learning/05 - Typedef.cpp
@@ -3,10 +3,10 @@
 
 // basic example of a vector and what typedef is used for 
 //typedef std::vector<std::pair<std::string, int>> pairlist_t; 
-// the name_t to show the nickname and shows the code that it is a typedef
+// the name_t to show the nickname and shows the coder that it is a typedef
 
-typedef std::string text_t;
-typedef int number_t;
+//typedef std::string text_t;
+//typedef int number_t;
 
 // might also see the following
 using text_t = std::string;
@@ -18,6 +18,9 @@ int main(){
         typedef = reserved keyword used to create an additional name (alias)
         for another data type. New identifier for an existing type,
         helps with readability and reduces typos.
+
+        Use when there is a clear benefit
+        Replaced with 'using' (work better w/ templates)
     */
 
     text_t firstName = "Bro";
