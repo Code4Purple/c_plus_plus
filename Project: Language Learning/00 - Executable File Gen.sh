efd841b8e9c4700549f4fa5ff8bb6712b23dfdf2
Code4Purple <1pokealex@gmail.com>
2026-04-15 12:03:47 -0500

    09 is done

--- Project: Language Learning/00 - Executable File Gen.sh
+++ Project: Language Learning/00 - Executable File Gen.sh
@@ -10,10 +10,10 @@ g++ -o "02 - Variables & Basic Data Types".exe "02 - Variables & Basic Data Type
 g++ -o "03 - Const Keyword".exe "03 - Const Keyword".cpp
 g++ -o "04 - Namespaces".exe "04 - Namespaces".cpp
 g++ -o "05 - Typedef & type aliases".exe "05 - Typedef & type aliases".cpp
-g++ -o 06\ -\ Arithmetic\ Operators.exe 06\ -\ Arithmetic\ Operators.cpp 
-g++ -o 07\ -\ Type\ conversion.exe 07\ -\ Type\ conversion.cpp
-g++ -o 08\ -\ User\ input.exe 08\ -\ User\ input.cpp
-
+g++ -o "06 - Arithmetic Operators.exe" "06 - Arithmetic Operators.cpp" 
+g++ -o "07 - Type conversion.exe" "07 - Type conversion.cpp"
+g++ -o "08 - User input.exe" "08 - User input.cpp" 
+g++ -o "09 - Useful math related functions.exe" "09 - Useful math related functions.cpp"
 
 for FILE in *.exe; do
     echo ".EXE File Created: $FILE"
--- Project: Language Learning/09 - Useful math related functions.cpp
+++ Project: Language Learning/09 - Useful math related functions.cpp
@@ -1,7 +1,34 @@
 #include <iostream>
+#include <cmath>
 
 int main(){
 
+    double x = 3;
+    double y = 4;
+    double z;
+
+    z = std::max(x,y); 
+    std::cout << "Max: " << z << '\n';
+    z = std::min(x,y);
+    std::cout << "Min: " << z << '\n';
+
+    z = pow(2,3); // 2 to the power of 3
+    std::cout << "pow: " << z << '\n';
+
+    z = sqrt(y); // square root
+    std::cout << "sqrt: " << z << '\n';
+
+    z = abs(x-y); // absolute 
+    std::cout << "abs: " << z << '\n';
+
+    z = round(3.4); // round to nearest whole number
+    std::cout << "round: " << z << '\n';
+
+    z = ceil(3.4); // round up
+    std::cout << "celi: " << z << '\n';
+
+    z = floor(3.99); // round down 
+    std::cout << "floor: " << z << '\n';
 
     return 0;
 }
