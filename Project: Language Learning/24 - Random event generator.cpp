ac06c8fcf662e3acc78fe233117b2dcb560b9208
Bennie Kowalski <65255810+Code4Purple@users.noreply.github.com>
2026-05-15 02:16:52 -0500

    Update 24 - Random event generator.cpp
    Add 
            + #include <ctime> @ Line 2
            + Basic example of random event gen

--- Project: Language Learning/24 - Random event generator.cpp
+++ Project: Language Learning/24 - Random event generator.cpp
@@ -1,7 +1,28 @@
-#include <iostream>
+#include <iostream>.
+#include <ctime>
 
 int main(){
 
+    srand(time(0))
+    int random_number = ramd() % 5 + 1;
+
+    switch(random_number){
+        case 1:
+            std::cout << "You win a bumper sticker!\n";
+            break;
+        case 2:
+            std::cout << "You win a t-shirt!\n";
+            break;
+        case 3:
+            std::cout << "You win a free lunch!\n";
+            break
+        case 4: 
+            std::cout << "You win a car!\n";
+            break;
+        case 5:
+            std::cout << "You win a gift card!\n";
+            break;
+    }
 
     return 0;
 }
