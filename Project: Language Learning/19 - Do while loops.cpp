37d330ff74e386d543c9471e4b8e149057d41430
Bennie Kowalski <65255810+Code4Purple@users.noreply.github.com>
2026-05-15 00:08:15 -0500

    Update 19 - Do while loops.cpp
    Basic do while loop example is DONE!

--- Project: Language Learning/19 - Do while loops.cpp
+++ Project: Language Learning/19 - Do while loops.cpp
@@ -1,7 +1,26 @@
 #include <iostream>
 
 int main(){
+    /*
+        do while loops - do a block of code first,
+                                    THEN repeat again if condition is true
+    */
 
+    int number;
 
+    // Basic Example of the While Loop 
+    // This Keeps going until a postive number is reach for intake answer
+    /*while(number < 0){
+        std::cout << "Enter a positive #: ";
+        std::cin >> number;
+    }*/
+
+    // do while loop -- version
+    do{
+        std::cout << "Enter a positive #: ";
+        std::cin >> number;
+    }while(number < 0)
+
+    std::cout << "The # is: " << number;
     return 0;
 }
