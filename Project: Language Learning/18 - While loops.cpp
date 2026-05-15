15184d9a63263eb9515b7386885fe912b7f57ead
Bennie Kowalski <65255810+Code4Purple@users.noreply.github.com>
2026-05-14 23:49:52 -0500

    Update 18 - While loops.cpp
    Finish Basic Example of a While Loop

--- Project: Language Learning/18 - While loops.cpp
+++ Project: Language Learning/18 - While loops.cpp
@@ -2,6 +2,15 @@
 
 int main(){
 
+    std::string name;
 
+    // This will keep asking the user to put in a name into the system 
+    while(name.empty()){
+        std::cout << "Enter your name: ";
+        std::getline(std::cin,name);
+    }
+    std::cout << "Hello, " << name << std::endl;
+    
+    
     return 0;
 }
