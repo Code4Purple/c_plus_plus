58ce652bfc2880c4e2988f28cbe57e34cecf4dad
Bennie Kowalski <65255810+Code4Purple@users.noreply.github.com>
2026-05-15 18:30:27 -0500

    Update 26 - User defined functions.cpp
    Add 
          + whiteRabbit();
          + basic setup of a function

--- Project: Language Learning/26 - User defined functions.cpp
+++ Project: Language Learning/26 - User defined functions.cpp
@@ -1,7 +1,36 @@
 #include <iostream>
 
+// void whiteRabbit(); 
+
+        //or
+
+void whiteRabbit(){
+    std::cout << "Wake up Neo...\n";
+    std::cout << "The Matrix has you...\n";
+    std::cout << "Follow the White rabbit\n";
+    std::cout << "Knock, Knock, Neo..." << std::endl;
+}
+
 int main(){
+    /*
+        functions = a block of reuseable code
 
+    */
 
+    // The function must come before the main() for this to run 
+    // The function can just be declared or written out before the main() 
+    whiteRabbit();
+    
     return 0;
 }
+
+/*
+    // Use this after the 1st choice above
+    void whiteRabbit(){
+        std::cout << "Wake up Neo...\n";
+        std::cout << "The Matrix has you...\n";
+        std::cout << "Follow the White rabbit\n";
+        std::cout << "Knock, Knock, Neo..." << std::endl;
+}
+
+*/
