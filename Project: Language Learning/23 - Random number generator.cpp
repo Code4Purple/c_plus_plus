4b00c5a3cbe4db2ce19625a5cfa208bf247f8444
Bennie Kowalski <65255810+Code4Purple@users.noreply.github.com>
2026-05-15 02:04:44 -0500

    Update 23 - Random number generator.cpp
    Add 
           + Basic Setup of the Random Number Gen
           + Basic Console Print out the Gen
           + Example of 3 Dice was rolled for a Game with the Gen

--- Project: Language Learning/23 - Random number generator.cpp
+++ Project: Language Learning/23 - Random number generator.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
 
 int main(){
+    /*
+        pseudo-random = NOT truly random (but close)
+    */
 
+    srand(time(NULL));
+
+    // Basic Random Number Gen
+    int num = rand();
+    std::cout << num << std::endl;
+
+    // Rolled 3 Dice for 3 random numbers
+    int blue_dice = (rand() % 6) + 1;
+    int red_dice = (rand() % 6) + 1;
+    int green_dice = (rand() % 6) + 1;
+
+    std::cout << "Blue Dice:  " << blue_dice << ', ';
+    std::cout << "Red Dice:   " << red_dice << ', ';
+    std::cout << "Green Dice: " << green_dice << std::endl;
 
     return 0;
 }
