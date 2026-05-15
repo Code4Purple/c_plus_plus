4c51c8f00e2072e59b8f1d7d3388242a4f69cf46
Bennie Kowalski <65255810+Code4Purple@users.noreply.github.com>
2026-05-15 00:34:38 -0500

    Update 21 - Break & continue.cpp
    Basic Example of the Break and Continue for loops

--- Project: Language Learning/21 - Break & continue.cpp
+++ Project: Language Learning/21 - Break & continue.cpp
@@ -2,6 +2,22 @@
 
 int main(){
 
+    /*
+        break     = breaks out of a loop
+        continue  = skip the current iteration
+    */
+
+    // Tells the user what buttons are on the elavator (after 911)
+    for(int i = 1; i <= 15; i++){
+        if(i == 13){
+            // Since 911 a Floor 13 is bad luck in the USA
+            // wE want to skip the 13 but let the user see the rest of the buttons
+            continue; 
+        }
+        std::cout << "Button: " << i << std::endl;
+    }
+
+    
 
     return 0;
 }
