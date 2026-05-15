c58132c965deae3ece8e026adc2daa27d6790474
Bennie Kowalski <65255810+Code4Purple@users.noreply.github.com>
2026-05-15 16:25:25 -0500

    Update 25 - Number guessing game.cpp
    Game has been made can be sent to be tested!

--- Project: Language Learning/25 - Number guessing game.cpp
+++ Project: Language Learning/25 - Number guessing game.cpp
@@ -2,6 +2,32 @@
 
 int main(){
 
+    int num;
+    int guess;
+    int tries;
 
+    srand(time(null));
+    num = (rand() % 100) + 1;
+    
+    std::cout << "********* NUMBER GUESSING GAME *********" << std::endl;
+
+    do{
+        std::cout << "Enter a guess between (1-100): ";
+        std::cin >> guess;
+        tries++;
+
+        if(guess > num){
+            std::cout << "Too High!\n";
+        }
+        else if(guess < num){
+            std::cout << "Too Low!\n";
+        }
+        else{
+            std::cout << "CORRECT! # of tries: " << tries << '\n';
+        }
+    }while(guess != num);
+        
+    std::cout << "****************************************" << std::end;
+    
     return 0;
 }
