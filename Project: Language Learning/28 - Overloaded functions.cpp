2f5ff2684704ef35e4f16038c110f16a4d983d1b
Bennie Kowalski <65255810+Code4Purple@users.noreply.github.com>
2026-05-16 02:14:43 -0500

    Update 28 - Overloaded functions.cpp
    Add 
          + Basic Example without notes

--- Project: Language Learning/28 - Overloaded functions.cpp
+++ Project: Language Learning/28 - Overloaded functions.cpp
@@ -1,7 +1,33 @@
 #include <iostream>
 
+void whiteRabbit();
+void whiteRabbit(std::string name);
+void whiteRabbit(std::string name1, std::string name2);
+
 int main(){
 
-
+    whiteRabbit();
+    whiteRabbit("Morpheus");
+    whiteRabbit("Agent Smith", "Agent Brown");
+    
     return 0;
+}
+
+void whiteRabbit(){
+    std::cout << "Wake up, Neo...\n";
+    std::cout << "The Matrix hasss you...\n";
+    std::cout << "Follow the white rabbit.\n";
+    std::cout << "Knock, Knock, Neo.\n\n";
+}
+void whiteRabbit(std::string name){
+    std::cout << "Wake up, " << name << "...\n";
+    std::cout << "The Matrix hasss you...\n";
+    std::cout << "Follow the white rabbit.\n";
+    std::cout << "Knock, Knock, " << name << ".\n\n";
+}
+void whiteRabbit(std::string name1, std::string name2){
+    std::cout << "Wake up, " << name1 << " & " << name2 << "...\n";
+    std::cout << "The Matrix hasss you both...\n";
+    std::cout << "Follow the white rabbit.\n";
+    std::cout << "Knock, Knock, " << name1  << " & " << name2 << ".\n";
 }
