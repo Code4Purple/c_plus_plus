cadf2e3363a5d22938c5bc7f416faaff96fbbb4d
Bennie Kowalski <65255810+Code4Purple@users.noreply.github.com>
2026-05-16 17:33:10 -0500

    Update 31 - ROCK PAPER SCISSORS game.cpp
    player(); -- Created

--- Project: Language Learning/31 - ROCK PAPER SCISSORS game.cpp
+++ Project: Language Learning/31 - ROCK PAPER SCISSORS game.cpp
@@ -1,7 +1,48 @@
 #include <iostream>
+#include <cctype> // required for std::tolower
+
+char getUserChoice();
+char getComputerChoice();
+void showChoice(char choice);
+void chooseWinner(char player, char computer);
 
 int main(){
-    
+    char player;
+    char computer;
 
+    player = getUserChoice();
+
+    return 0;
+}
+
+
+char getUserChoice(){
+    char player;
+    std::cout << "********************************\n";
+    std::cout << "    Rock-Paper-Scissors Game!   \n";
+    std::cout << "********************************\n";
+    do{
+        std::cout << "| r - rock | ";
+        std::cout << "p - paper | ";
+        std::cout << "s - Scissors |\n";
+        std::cout << "player selection: ";
+        std::cin >> player;
+        // Convert and update the variable
+        player =  static_cast<char>(std::tolower(letter));
+            
+    }while(player != 'r' && player != 'p' && player != 's');
+
+    return player;
+}
+
+char getComputerChoice(){
+    return 0;
+}
+
+void showChoice(char choice){
+    return 0;
+}
+
+void chooseWinner(char player, char computer){
     return 0;
 }
