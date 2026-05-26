d557515e887d75ab3bae6788fe94b8f6d1152eff
Code4Purple <1pokalex@gmail.com>
2026-05-26 13:33:49 -0500

    Arrays --DONE

--- Project: Language Learning/32 - Arrays.cpp
+++ Project: Language Learning/32 - Arrays.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 
 int main(){
 
@@ -9,13 +10,35 @@ int main(){
      */
 
     // basic variable
-    std::string scar = "Corvette";
+    std::string scar = "camry";
     // basic array
     std::string car[] = {"Corvette", "Mustang", "Camry"};
 
-    std::cout << car[0] << '\n';
-    std::cout << car[1] << '\n';
+    std::cout << "Cars (Array 1)" << "\n";
+    std::cout << car[0] << ", ";
+    std::cout << car[1] << ", ";
     std::cout << car[2] << '\n';
 
+    // Another way to do due a basic array
+    std::string dogNames[3]; 
+    dogNames[0] = "Zues"; 
+    dogNames[1] = "Mario"; 
+    dogNames[2] = "Nemo"; 
+    
+    std::cout << "Dog Names (Array 2)\n"; 
+    std::cout << dogNames[0] << ", ";
+    std::cout << dogNames[1] << ", ";
+    std::cout << dogNames[2] << '\n';
+
+    // 3rd way to do arrays
+    std::string carSize[3];
+    carSize[0] = "sedan";
+    carSize[1] = "suv";
+    carSize[2] = "truck";
+
+    std::cout << "Car Size (Array 3)" << "\n";
+    std::cout << carSize[0] << ", ";
+    std::cout << carSize[1] << ", ";
+    std::cout << carSize[2] << '\n';
     return 0;
 }
