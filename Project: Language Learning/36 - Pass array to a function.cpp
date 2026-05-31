b5e04fe69666c573d5461e64bef0824816dd89ac
Bennie Kowalski <65255810+Code4Purple@users.noreply.github.com>
2026-05-31 15:36:42 -0500

    Update 36 - Pass array to a function.cpp
    + example of array pass threw into a function

--- Project: Language Learning/36 - Pass array to a function.cpp
+++ Project: Language Learning/36 - Pass array to a function.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
 
+double getTotal(double prices[]);
+
 int main(){
 
+    double prices[] = {49.49, 15.05, 75, 9.99};
+    double total = getTotal(prices);
+    std::cout << "$" << total;
 
     return 0;
+}
+
+double getTotal(double prices[]){
+    double sum = 0;
+
+    for(int i = 0; i < sizeof(prices)/sizeof(prices[0]); i++){
+        sum += prices[i];
+    }
+
+    return sum;
 }
