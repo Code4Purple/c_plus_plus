afd10dc39e7492458a7b1e542a15ca8612c6415c
Bennie Kowalski <65255810+Code4Purple@users.noreply.github.com>
2026-05-31 21:42:54 -0500

    Update 37 - Search an array for an element.cpp
    // commented out the int 
    
    + added --  string example

--- Project: Language Learning/37 - Search an array for an element.cpp
+++ Project: Language Learning/37 - Search an array for an element.cpp
@@ -4,21 +4,30 @@ int searchArray(int array[], int size, int element);
 
 int main(){
 
-    int numbers[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    int size = sizeof(numbers)/sizeof(number[0]);
+    //int numbers[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    std::string heros[] = {"Batman", "Superman", "The Flash"};
+    
+    //int size = sizeof(numbers)/sizeof(number[0]);
+    int size = sizeof(heros)/sizeof(heros[0]);
+    
     int index;
-    int myNum;
-
+    
+    //int myNum;
+    std::string myString;
+    
     std::cout << "Enter element to search for: ";
-    std::cin >> myNum;
-
-    index = searchArray(numbers, size, myNum);
-
+    //std::cin >> myNum;
+    std::cin >> myString; 
+    
+    //index = searchArray(numbers, size, myNum);
+    index = searchArray(heros,size,myString);
+    
     if(index != -1){
-        std::cout << myNum << " is at index " << index;
+        //std::cout << myNum << " is at index " << index;
+        std::cout << myString << " is at index " << index;
     }
     else {
-        std::cout << myNum << " is not in the array";
+        std::cout << myString << " is not in the array";
     }
 
     return 0;
@@ -33,5 +42,4 @@ int searchArray(int array[], int size, int element){
     }
     return -1;
 
-    // save https://youtu.be/-TkoO8Z07hI?t=11959
 }
