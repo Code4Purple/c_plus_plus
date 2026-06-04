2c464e1f707e27f18fde3de09ce170287d2c84d6
Code4Purple <1pokealex@gmail.com>
2026-06-04 11:50:02 -0500

    39 - Fill() function --- DONE

--- Project: Language Learning/39 - Fill() function.cpp
+++ Project: Language Learning/39 - Fill() function.cpp
@@ -2,6 +2,25 @@
 
 int main(){
 
+    /*
+        fill() = Fills a range of elements with a specified value
+                    fill(begin, end, value)
+    */
+
+    const int size = 600;
+    std::string drinks[size];
+
+    fill(drinks, drinks + (size/3), "Pespi");
+    fill(drinks + (size/3), drinks + (size/3)*2, "Mountain Dew");
+    fill(drinks + (size/3)*2, drinks + size, "Starry");
+
+    int i = 0;
+    for(std::string drink : drinks){
+        i++;
+        std::cout << i << ". "<< drink << "\n";
+        
+    }
+
 
     return 0;
 }
