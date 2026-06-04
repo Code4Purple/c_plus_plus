f2891f7daf64c358b1403042d2bec1f190b1d800
Code4Purple <1pokalex@gmail.com>
2026-06-04 14:25:31 -0500

    40 - Fill an array with user input --- DONE

--- Project: Language Learning/40 - Fill an array with user input.cpp
+++ Project: Language Learning/40 - Fill an array with user input.cpp
@@ -2,7 +2,23 @@
 
 int main(){
 
-    // bookmark : https://youtu.be/-TkoO8Z07hI?t=12681
+    std::string heros[5];
+    int size = sizeof(heros)/sizeof(heros[0]);
 
+    for(int i = 0; i < size; i++){
+        std::cout << "Enter a Superhero #" << i + 1 << ": ";
+        getline(std::cin,heros[i]);
+        if(heros[i] == "q"){
+            break;
+        }
+    }
+    
+    std::cout << "Your superheros team of 5 are..." << std::endl;
+    for (std::string hero : heros){
+        if(hero == "q" ){
+            break;
+        }
+        std::cout << "---> " << hero << std::endl;
+    }   
     return 0;
 }
