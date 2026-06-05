68a15e12b000175ef18b56451aef4f303037d175
Bennie Kowalski <65255810+Code4Purple@users.noreply.github.com>
2026-06-05 01:05:33 -0500

    -- Ready 4 Testing
    + Quiz Game Coded
    -- Ready For testing!

--- Project: Language Learning/42 - QUIZ GAME.cpp
+++ Project: Language Learning/42 - QUIZ GAME.cpp
@@ -2,6 +2,49 @@
 
 int main(){
 
-    // save : https://youtu.be/-TkoO8Z07hI?t=13558
+    std:: string questions[] = {"1. What year was C++ created?",
+                                "2. Who invented C++?",
+                                "3. What is the predecessor of C++?",
+                                "4. is the Earth flat?"};
+    std::string options[][4] = {{"A. 1969","B. 1975","C. 1985","D. 1989"},
+                                {"A. Guido van Rossum","B. Bjarne Stroustrup", "C. John Carmack", "D. Mark Zuckerburg"},
+                                {"A. C", "B. C+", "C. C--","D. B++"},
+                                {"A. yes", "B. no", "C. sometimes", "D. What's Earth?"}};
+
+    char answerKry[] = {'C', 'B', 'A','B'};
+
+    int sizeQ = sizeof(questions)/sizeof(questions[0]);
+    char guess;
+    int score;
+
+    for(int i = 0; i < size; i++){
+        std::cout << "***********************************\n";
+        std::cout << questions[i] << '\n';
+        std::cout << "***********************************\n";
+
+        for(int j = 0; j < sizeof(options[i])/sizeof(options[i][0]); j++){
+            std::cout << options[i][j] << '\n';
+        }
+        std::cin >> guess;
+        guess = toupper(guess)
+
+        if(guess == answerKey[i]){
+            std::cout << "✅\n";
+            score++;
+        }
+        else{
+            std::cout << "❌\n";
+            std::cout << "Correct Answer: " << answerKey[i] << '\n';
+        }
+    }
+
+    std::cout << "***********************************\n";
+    std::cout << "*            FINAL SCORE            *";
+    std::cout << "***********************************\n";
+    std::cout << "✅ Answers : " << score << '\n';
+    std::cout << "# of ❓    :" << size << '\n';
+    std::cout << "Score       : " << (score/(double)size)*100 << "%"; 
+    std::cout << "***********************************\n";
+    
     return 0;
 }
