01329b1d6dfe2446d093d33dc9e93f0ebf812f16
Bennie Kowalski <65255810+Code4Purple@users.noreply.github.com>
2026-06-05 02:06:11 -0500

    Update 43 - Memory addresses.cpp
    + Notes Added
    + Bookmark Noted

--- Project: Language Learning/43 - Memory addresses.cpp
+++ Project: Language Learning/43 - Memory addresses.cpp
@@ -2,6 +2,10 @@
 
 int main(){
 
-
+    /*
+        memory address = a location in memory where the data is stored
+        a memory address can accessed with & (address-of operator)
+    */
+    // Bookmark: https://youtu.be/-TkoO8Z07hI?t=14266
     return 0;
 }
