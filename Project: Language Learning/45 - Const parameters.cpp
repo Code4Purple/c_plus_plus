9af5caaa68d5ee37d13b7f5209ea4dcd5462a5bb
Bennie Kowalski <65255810+Code4Purple@users.noreply.github.com>
2026-06-06 18:40:59 -0500

    Update 45 - Const parameters.cpp
    save

--- Project: Language Learning/45 - Const parameters.cpp
+++ Project: Language Learning/45 - Const parameters.cpp
@@ -1,12 +1,24 @@
 #include <iostream>
 
+void printInfo(std::string name, int age);
+
 int main(){
 
     /*
-        const parameter = 
+        const parameter = parameter that is effectively read-only 
+                            code is more secure & conveys intent
+                            useful for references and pointers
     */
 
-    // Bookmark : https://youtu.be/-TkoO8Z07hI?t=14691
+    std::sting name = "Batmam";
+    int age = 35;
 
+    printInfo(nmae, age);
+    https://youtu.be/-TkoO8Z07hI?t=14853
     return 0;
+}
+
+void printInfo(std::string name, int age){
+    std::cout << name << '\n';
+    std::cout << age << '\n';
 }
