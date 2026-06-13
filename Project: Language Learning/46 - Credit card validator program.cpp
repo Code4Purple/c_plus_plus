4e4e1beee787011a0503def68ba3075c89029fc8
Bennie Kowalski <65255810+Code4Purple@users.noreply.github.com>
2026-06-12 22:30:50 -0500

    Update 46 - Credit card validator program.cpp
    save state

--- Project: Language Learning/46 - Credit card validator program.cpp
+++ Project: Language Learning/46 - Credit card validator program.cpp
@@ -1,8 +1,34 @@
 #include <iostream>
 
+int getDigit(const int number);
+int sumOddDigits(const std::string cardNumber);
+int sumEvenDigits(const std::string cardNumber);
+
 int main(){
 
-    // https://youtu.be/-TkoO8Z07hI?t=14875
+    // Used the Luhn Alogorithm
 
+    std::string cardNumber;
+    int result = 0;
+
+    std::cout << "Enter a credit card #: ";
+    std::cin >> cardNumber;
+
+    result = sumEvenDigits(cardNumber) + sumOddDigits(cardNumber);
+    
+    
     return 0;
+}
+
+int getDigit(const int number){
+    return 0;
+}
+int sumOddDigits(const std::string cardNumber){
+    return 0;
+}
+int sumEvenDigits(const std::string cardNumber){
+    int sum = 0;
+
+    // Bookmark :  https://youtu.be/-TkoO8Z07hI?t=15158
+    
 }
