78abaf67c27dd385bd16915f604bd9975beb53fb
Bennie Kowalski <65255810+Code4Purple@users.noreply.github.com>
2026-06-20 19:19:51 -0500

    READY FOR TEST
    + notes
    + code
    
    --- Ready for test ----

--- Project: Language Learning/48 - Null pointers.cpp
+++ Project: Language Learning/48 - Null pointers.cpp
@@ -2,6 +2,31 @@
 
 int main(){
 
+    /*
+        Null value = a special value that means something has no value.
+                        When a pointer is holding a null value,
+                        that pointer is not pointing at anything (null pointer)
+
+        nullptr = keyword represents a null pointer literal
+
+        nullptrs are helpful when determining if an address was successfully assigned to a pointer.
+
+        When using pointers, be careful that your code isn't 
+        dereferencing nullptr or pointing to free memory this will
+        cause undefined behavior.
+    */
+
+    int *pointer = nullptr;
+    int x = 1234;
+
+    pointer = &x;
+
+    if(pointer == nullptr){
+        std::cout << "ERROR ADDRESS WASNT ASSGINED" << "\n";
+    }
+    else{
+        std::cout << "Address was assgined!\n"
+    }
 
     return 0;
 }
