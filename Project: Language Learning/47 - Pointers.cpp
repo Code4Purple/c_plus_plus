5f71c97bfc6c4e7fe34ac36a9db277f1d2329302
Bennie Kowalski <65255810+Code4Purple@users.noreply.github.com>
2026-06-20 19:01:16 -0500

    READY FOR TEST
    + Notes
    + Code 
    
    --- READY FOR TRST ------

--- Project: Language Learning/47 - Pointers.cpp
+++ Project: Language Learning/47 - Pointers.cpp
@@ -2,7 +2,22 @@
 
 int main(){
 
-    // BOOKMARK : https://youtu.be/-TkoO8Z07hI?t=15476
+    /* 
+        pointers = variable that stores a memory address of another variable
+                    sometimes it's easier to work with an address
+        
+        & = address-of operator
+        * = dereference operator
+    */
+
+    std::string name = "Batman"
+    std::string *pName = &name;
+    std::cout << pName;
+
+    std::cout << pName << "\n"; // This will give you an address for the memory 
+    std::cout << *pName << "\n"; // This will give you what the memoery adress is 
+
+    
 
     return 0;
 }
