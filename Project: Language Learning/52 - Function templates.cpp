e4b6407b1a7c6b9761cd68fca40514b9c6e06841
Code4Purple <1pokealex@gmail.com>
2026-07-09 11:16:42 -0500

    52 - Notes and example are both working!

--- Project: Language Learning/52 - Function templates.cpp
+++ Project: Language Learning/52 - Function templates.cpp
@@ -1,7 +1,29 @@
 #include <iostream>
+template <typename T>
+T max(T x, T y){
+    return (x > y) ? x : y;
+};
+//int max(int x, int y);
 
 int main(){
 
+    /*
+        function templates = describes what a function looks like.
+                             Can be used to generate as many overloaded functions
+                             as needed, each using different data types
+    */
+
+    std::cout << max(1,2) << "\n";
 
     return 0;
 }
+/*  We would normally use an overload function to max out each datatype
+    but if we use templates we dont need all the different datatypes listed.
+*/
+/*int max(int x, int y){
+    return (x > y) ? x : y;
+}
+double max(double x, double y){
+    return (x > y) ? x : y;
+} */
+
