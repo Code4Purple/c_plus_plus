2eca1838e87414b93b2538814e4b91faa930eb27
Code4Purple <1pokealex@gmail.com>
2026-07-09 11:04:29 -0500

    51 is done with notes and working example!

--- Project: Language Learning/51 - Recursion.cpp
+++ Project: Language Learning/51 - Recursion.cpp
@@ -1,9 +1,39 @@
 #include <iostream>
 
+void walk(int steps);
+
 int main(){
 
-   // bookmark : https://youtu.be/-TkoO8Z07hI?t=17538
+    /*
+    
+        recursion = a programming technique where a function
+                    invokes itself from within
+                    break a complex concept into a repeatable single step
 
+        (iterative vs recursive)
+        
+        advantages = less code and it is cleaner
+                     useful for sorting and searching algorithms
+        
+        disadvantages = uses more memory
+                        slower
+
+        for example : 
+
+            void doSomething(){
+                doSomething();
+            }
+    
+    */    
+
+    walk(100);
 
     return 0;
+}
+
+void walk(int steps){
+    if(steps > 0){
+        std::cout << "You take a step!\n";
+        walk(steps - 1);
+    }
 }
