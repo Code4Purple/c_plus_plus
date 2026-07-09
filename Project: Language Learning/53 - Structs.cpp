307156760462aa71136c2d505bfcf29b45519485
Code4Purple <1pokealex@gmail.com>
2026-07-09 11:34:28 -0500

    53 - Notes & Example is working!

--- Project: Language Learning/53 - Structs.cpp
+++ Project: Language Learning/53 - Structs.cpp
@@ -1,7 +1,33 @@
 #include <iostream>
 
+struct hero{
+    std::string name;
+    bool hasPowers;
+};
+
 int main(){
 
+    /*
+        struct = A structure that group related variables under one name
+                 structs can contain many different data types 
+                 (string, int, double, bool, etc.)
+                 variables in a struct are known as "members"
+                 members can be access with. "Class member Access Operator"
+    */
+
+    hero batman;
+    batman.name = "Bruce Wayne";
+    batman.hasPowers = false;
+
+    hero TheFlash;
+    TheFlash.name = "Berry Allen";
+    TheFlash.hasPowers = true;
+
+    std::cout << batman.name << "\n";
+    std::cout << batman.hasPowers << "\n";
+
+    std::cout << TheFlash.name << "\n";
+    std::cout << TheFlash.hasPowers << "\n";
 
     return 0;
 }
