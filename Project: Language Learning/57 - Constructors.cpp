3b36ca7f6c6913df1ec1d54225dbf0d7c02c8009
Code4Purple <1pokealex@gmail.com>
2026-07-12 10:49:54 -0500

    57 is done, +NOTES , +EXAMPLE

--- Project: Language Learning/57 - Constructors.cpp
+++ Project: Language Learning/57 - Constructors.cpp
@@ -1,7 +1,36 @@
 #include <iostream>
 
+class hero{
+    public:
+        std::string real_name;
+        std::string superhero_name;
+        bool fighting_crime;
+    
+    hero(std::string realID, std::string superID, bool active){
+        this->real_name = realID;
+        this->superhero_name = superID;
+        this->fighting_crime = active;
+    }
+        
+};
+
+void printAttributes(hero info);
+
 int main(){
 
+    /*
+        constructor = special method that is automatically called when an object is
+        instantiated useful for assigning values to attributes as arguments.
+    */
+
+    hero batman("Bruce Wayne", "Batman", true);
+    printAttributes(batman);
 
     return 0;
+}
+
+void printAttributes(hero info){
+    std::cout << info.real_name << "\n";
+    std::cout << info.superhero_name << "\n";
+    std::cout << info.fighting_crime << "\n";
 }
