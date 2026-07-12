b3082c7e8c324e7790fa6a75584d586ece7aaf1e
Code4Purple <1pokealex@gmail.com>
2026-07-12 10:23:45 -0500

    56 is done with notes and example!

--- Project: Language Learning/56 - Object Oriented Programming.cpp
+++ Project: Language Learning/56 - Object Oriented Programming.cpp
@@ -1,8 +1,51 @@
 #include <iostream>
 
+class Human{
+    public:
+        std::string name;
+        std::string occupation;
+        int age;
+
+        void eat(){
+            std::cout << "This " << name <<" is eating\n";
+        }
+        void drink(){
+            std::cout << "This " << name << " is drinking\n";
+        }
+        void sleep(){
+            std::cout << "This " << name << " is sleeping\n";
+        }
+};
+
 int main(){
 
-    // Bookmarked : https://youtu.be/-TkoO8Z07hI?t=19119
+    /*
+        object = A collection of attributes and methods
+                 They can have characteristics and could peform actions
+                 Can be used to mimic read world items (ex. Phone, Book, Dog)
+                 Created from a class which acts as a "blue-print"
+    */
+
+    Human human1;
+    
+    human1.name = "Rick";
+    human1.occupation = "scientist";
+    human1.age = 70;
+    
+    human1.eat();
+    human1.drink();
+    human1.sleep();
+
+    std::cout << "\n";
+
+    Human human2;
+    human2.name = "Morty";
+    human2.occupation = "student";
+    human2.age = 14;
+
+    human2.eat();
+    human2.drink();
+    human2.sleep();
 
     return 0;
 }
